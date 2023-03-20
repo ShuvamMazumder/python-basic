649f3e6d3628d4142d6f2dddcb58007dcf188df8
Shuvam Mazumder <shuvam.baiust@gmail.com>
2023-03-21 01:43:33 +0600

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,150 @@
+# python practice 
+# Code with Harry
+
+
+# print('hello world')
+# print(5)
+# print("bye")
+# print(17*8)
+# print('try for a new\nline')
+# print("this is a \"backslash\" line")
+# print("hey",6)
+#
+#
+# a=1
+# b=True
+# c="shuvam"
+# d=None
+# e=2.2
+# f=complex(8, 2)
+# print("type of",a, type(a))
+# print("type of",b, type(b))
+# print("type of",c, type(c))
+# print("type of",d, type(d))
+# print("type of",e, type(e))
+# print("type of",f, type(f))
+#
+# List = [8, 2.3 , [4,-5],['apple']]
+# tuple = (('hi','hello','bye'))
+# print(List)
+# print(tuple)
+#
+#
+# sum = 0
+# for i in range(101):
+#     if i % 2 != 0:
+#         sum += i
+# print(sum)
+#
+#
+# my_list = [1, 5, 6, 5, 1, 2, 3]
+# duplicates = [i for i in set(my_list) if my_list.count(i) > 1]
+#
+# print(duplicates)
+#
+#
+# numbers= [1,2,3,4,5,6,7,8,9,10]
+# for num in numbers:
+#     result = num * 2
+#     print(result)
+#
+#
+# numbers= [1,2,3,4,5,6,7,8,9,10]
+# results = []
+#
+# for num in numbers:
+#     result = num * 2
+#     results.append(result)
+#
+# print(results)
+
+
+#fruit_seller = input("what is your favourite fruit?")
+#print(fruit_seller)
+
+#name = input("what is ur name?")
+#greetings = "Hello!"
+#print(greetings,name)
+
+# required_age_at_school = 5
+# hammad_age = int(input())
+# if hammad_age == required_age_at_school:
+#     print("hammad can join the school")
+# elif hammad_age > required_age_at_school:
+#     print("hammad can join the school")
+# elif hammad_age <= 0:
+#     print("doesn't exit")
+# elif hammad_age < required_age_at_school:
+#     print("he's still a baby")
+# else:
+#     print("hammad can't go to school")
+
+
+#functions
+
+# def fun_1():
+#     print("shuvam")
+#     print("shuvam")
+#     print("shuvam")
+#
+# fun_1()
+#
+# def fun_2():
+#     text="shuvam 2"
+#     print(text)
+#     print(text)
+#     print(text)
+# fun_2()
+#
+# def fun_3(text):
+#     print(text)
+#     print(text)
+#     print(text)
+# fun_3("shuvam 3")
+
+# def school_calculator(age):
+#     if age==5:
+#         print("can join")
+#     elif age>=5:
+#         print("can join")
+#     else:
+#         print("can't join")
+# school_calculator(5)
+
+# def future_age(age):
+#     new_age=age+10
+#     return new_age      #stored in future_age
+#
+# future_predicted_age = future_age(10)
+# print(future_predicted_age)
+
+#while loop
+# x=0
+# while (x<=5):
+#     print(x)
+#     x=x+1
+#for loop
+# for x in range(0,10):
+#     print(x)
+
+#for loop in array
+# days=['mon','tue','wed','thru','fri','sat','sun']
+# for d in days:
+#     #if(d=='fri'):break      #loop stops
+#     if(d=='fri'):continue   #skips
+#     print(d)
+
+import math
+print(math.pi)
+
+import statistics
+x=[10,20,40,50]
+print(statistics.mean(x))
+
+
+
+
+
+
+
+
